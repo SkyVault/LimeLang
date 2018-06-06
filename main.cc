f59b5e4993e4891406301f777b9621b4c7df14b9
Dustin <skyvaultgames@gmail.com>
2018-06-05 18:15:30 -0700

    Testing a new script

--- main.cc
+++ main.cc
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <cassert>
 #include <map>
 
 #include "ast.h"
@@ -17,6 +18,47 @@ other int = myVar + 2
 )LEAF"};
 
 #define RUN_TESTS
+
+double run_expression(Node* ast) {
+    double result{0.0};
+
+    assert(ast->type == LIME_NODE_EXPRESSION); 
+
+    auto op = ast->children[0];
+
+    assert(op->type == LIME_NODE_OPERATOR); 
+
+    auto a = op->children[0];
+    auto b = op->children[1];
+    
+    auto a_num{0.0}, b_num{0.0};
+
+    if (a->type == LIME_NODE_NUMBER_LITERAL)
+        a_num = std::stof(a->token.word);
+    
+    if (b->type == LIME_NODE_NUMBER_LITERAL)
+        b_num = std::stof(b->token.word);
+
+    if (a->type == LIME_NODE_EXPRESSION)
+        a_num = run_expression(a);
+    
+    if (b->type == LIME_NODE_EXPRESSION)
+        b_num = run_expression(b);
+    
+    if (op->token.op == LIME_PLUS_OPERATOR)
+        result = a_num + b_num;
+
+    if (op->token.op == LIME_MINUS_OPERATOR)
+        result = a_num - b_num;
+
+    if (op->token.op == LIME_MULTIPLICATION_OPERATOR)
+        result = a_num * b_num;
+
+    if (op->token.op == LIME_DIVISION_OPERATOR)
+        result = a_num / b_num;
+
+    return result;
+}
 
 int main() {
 #ifdef RUN_TESTS
@@ -37,6 +79,13 @@ int main() {
 
     // Ast
     const auto ast = create_ast_from_tokens(tokens);
+
+    for (const auto n : ast.children) {
+        if (n->type == LIME_NODE_VARIABlE_ASSIGNMENT) {
+            std::cout << n->identifier->word << " = ";
+            std::cout << run_expression(n->children[0]) << "\n";
+        }
+    }
 	
 	out.close();
 }
